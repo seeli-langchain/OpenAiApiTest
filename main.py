b8b0e70b13f22a592bfeedfa0011415f79148dec
markusseeli <112763637+markusseeli@users.noreply.github.com>
2024-01-08 11:44:27 +0100

    Initial Checkin

---
+++ main.py
@@ -0,0 +1,32 @@
+import json
+from dotenv import load_dotenv
+import requests
+import os
+from openai import OpenAI
+
+
+load_dotenv()
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+
+client = OpenAI()
+
+response = client.chat.completions.create(
+  model="gpt-4-vision-preview",
+  messages=[
+    {
+      "role": "user",
+      "content": [
+        {"type": "text", "text": "What’s in this image?"},
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+          },
+        },
+      ],
+    }
+  ],
+  max_tokens=300,
+)
+
+print(response.choices[0])
